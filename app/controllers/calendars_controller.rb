7609495abe822344321a822971332ad885b7d56c
Office-asami <s-wakita@office-asami.com>
2022-05-17 23:18:08 +0900

    Issue4〜6の再修正
    Issue4〜6の再修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -36,15 +36,13 @@ class CalendarsController < ApplicationController
       end
       
       wday_today_num = Date.today.wday # wdayメソッドを用いて取得した数値
-      i = 0
-      wday_num = wday_today_num + i
+      wday_num = wday_today_num + x
       if wday_num >= 7 #「wday_numが7以上の場合」という条件式
         wday_num = wday_num -7
       end
 
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans, :wday => wdays[wday_num] }
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wdays[wday_num] }
       @week_days.push(days)
-      i += 1
     end
 
   end
